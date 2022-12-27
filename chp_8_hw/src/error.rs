21f46a5f9ae763f2ab0477d344b6a6e9965799f8
nuts-rice <msis44443@gmail.com>
2022-12-27 15:02:34 -0500

    🚧 wip: chap 8 hw

--- chp_8_hw/src/error.rs
+++ chp_8_hw/src/error.rs
@@ -1,11 +1,33 @@
-use anyhow;
+use anyhow::{ensure, Result};
 use thiserror::Error;
 
 use std::io;
 use std::string::FromUtf8Error;
 
-#[derive(Error, Debug)]
-pub enum Error {
+pub enum Cli {
+    Command(String),
+}
+
+#[derive(Debug, Error)]
+pub enum CliError {
     #[error("Unexpected command type `{0}`")]
     UnexpectedCommand(String),
+    #[error("IO error")]
+    UnexpectedIO(#[from] io::Error),
+    #[error("Unexpected operation (expected: {expected:?}. got {found:?}) ")]
+    UnexpectedOperation { expected: String, found: String },
 }
+
+fn get_cli() -> Result<String> {
+    unimplemented!()
+}
+
+/*
+impl fmt::Debug for Cli {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+
+        }
+    }
+}
+*/
