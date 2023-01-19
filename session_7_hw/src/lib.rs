619489af3aa59c8f58192a179a68cf57ecd6f2c8
nuts-rice <msis44443@gmail.com>
2023-01-19 16:17:39 -0500

    🧪 miller rabbin failing rn

--- session_7_hw/src/lib.rs
+++ session_7_hw/src/lib.rs
@@ -20,19 +20,38 @@ mod eea {
 }
 
 mod miller_rabin {
-    pub fn miller_rabin(_number: u64, _babses: &[u64]) -> u64 {
-        unimplemented!()
+    //returns 0 on prime _number
+    ////otherwise return witness
+    pub fn miller_rabin(_number: u64, bases: &[u64]) -> u64 {
+        if _number <= 4 {
+            match _number {
+                2 => return 0,
+                _ => return _number,
+            }
+        }
+        if bases.contains(&_number) {
+            return 0;
+        }
+        //n -1 = 2^k * q where q is odd
+        let k: u64 = (_number - 1).trailing_zeros() as u64;
+        let q = (_number - 1) >> k;
+        for base in bases {
+            if !is_prime_base(_number, *base, k, q) {
+                return *base;
+            }
+        }
+        0
     }
 
-    fn is_prime_base(number: u64, base: u64, two_power: u64, odd_power: u64) -> bool {
-        let mut x: u128 = modulo_power(base, odd_power, number) as u128;
-        let bnumber: u128 = number as u128;
-        if x == 1 || x == (bnumber - 1) {
+    fn is_prime_base(_number: u64, base: u64, two_power: u64, odd_power: u64) -> bool {
+        let mut x: u128 = modulo_power(base, odd_power, _number) as u128;
+        let b_number: u128 = _number as u128;
+        if x == 1 || x == (b_number - 1) {
             return true;
         }
         for _ in 1..two_power {
-            x = (x * x) % bnumber;
-            if x == (bnumber - 1) {
+            x = (x * x) % b_number;
+            if x == (b_number - 1) {
                 return true;
             }
         }
@@ -60,12 +79,25 @@ mod miller_rabin {
 
 #[cfg(test)]
 mod test {
-    use super::*;
+
     use crate::eea::extended_euclid;
+    use crate::miller_rabin::miller_rabin;
     #[test]
     fn eea_test() {
         let executed = extended_euclid(153, 87);
         assert_eq!((3, 4, -7), executed);
+    }
+
+    #[test]
+    fn miller_rabin_test() {
+        let primes = vec![3, 5, 7, 11, 13];
+        let mut product = 1;
+        let mut prime_flag = 0;
+        for p in &primes {
+            product *= p;
+            prime_flag |= miller_rabin(*p as u64, &primes);
+        }
+        assert_eq!(prime_flag, 0)
     }
 }
 
