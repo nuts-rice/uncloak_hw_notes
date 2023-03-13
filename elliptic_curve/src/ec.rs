6092e99803b971e8b5a320dcf8afd5ac9ef7a4eb
mishka_root <mishka_root@0x0f-softworks-librem.lan>
2023-03-13 15:09:47 -0400

    🚧 elliptic curve members

--- elliptic_curve/src/ec.rs
+++ elliptic_curve/src/ec.rs
@@ -17,17 +17,30 @@ struct Point {
     curve: EllipticCurve,
 }
 */
+
 pub use crypto_bigint as bigint;
 use num_bigint::BigUint;
-use std::fmt;
+use std::fmt::*;
 
 #[derive(Clone, Debug, Default, Eq, PartialEq, PartialOrd, Ord)]
-pub struct Sep256k1 {
-    P: BigUint,
-    Q: BigUint,
-    R: BigUint,
-    a: u32,
-    b: u32,
+pub struct EllipticCurve {
+    pub p: BigUint,
+    pub modulus: BigUint,
+    pub a2: usize,
+    pub a4: usize,
+    pub a6: usize,
+    pub is_weirstrass: bool,
+}
+#[derive(Debug, Clone, PartialEq)]
+pub struct Point {
+    pub point: Option<FinitePoint>,
+    pub curve: EllipticCurve,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct FinitePoint {
+    pub x: BigUint,
+    pub y: BigUint,
 }
 
 /*
@@ -38,17 +51,36 @@ impl EllipticCurve for Sep256k1 {
 }
 */
 
-pub trait EllipticCurve: 'static + Clone + Eq + Ord + Send + Sync {
-    type Uint: bigint::ArrayEncoding;
-    fn is_on_curve() {
+impl EllipticCurve {
+    fn new(p: BigUint, modulus: BigUint, coeffiecents: &[usize]) -> Self {
+        EllipticCurve {
+            p,
+            modulus,
+            a2: coeffiecents[0],
+            a4: coeffiecents[1],
+            a6: coeffiecents[2],
+            is_weirstrass: coeffiecents[0] == 0,
+        }
+    }
+
+    //discriminant is invariant quality of roots that holds true regardless of transformations
+    fn discriminant(&self) -> i128 {
+        let b2 = 4 * self.a2;
+        let (b4, b6, b8) = (2 * self.a4, 4 * self.a6, b2 * self.a6 - self.a4.pow(2));
+        let delta_summand = (9 * b2 * b4 * b6) as i128;
+        let delta_negand = (b8 * b2.pow(2) + 8 * b4.pow(3) + 27 * b6.pow(2)) as i128;
+        delta_summand - delta_negand
+    }
+
+    fn is_smooth(&self) -> bool {
+        self.discriminant() != 0
+    }
+
+    fn contains(&self, _point: &Point) -> bool {
         unimplemented!()
     }
 
-    fn to_bytes(&self) {
-        unimplemented!()
-    }
-
-    fn has_point(&self) {
+    fn contains_inner(&self, _point: &FinitePoint) -> bool {
         unimplemented!()
     }
 
@@ -60,12 +92,16 @@ pub trait EllipticCurve: 'static + Clone + Eq + Ord + Send + Sync {
     }
 }
 
-pub trait Point: 'static + Copy + Clone + Send + Sync {
-    type X: bigint::Integer;
-    type Y: bigint::Integer;
+impl FinitePoint {
+    fn reduce_modulo(self, modulus: &BigUint) -> Self {
+        Self {
+            x: self.x % modulus,
+            y: self.y % modulus,
+        }
+    }
+}
 
-    fn x(&self) -> Self::X;
-
+impl Point {
     fn to_bytes(&self) {
         unimplemented!()
     }
--- elliptic_curve/src/lib.rs
+++ elliptic_curve/src/lib.rs
@@ -1,1 +1,3 @@
+#![feature(once_cell)]
+
 pub mod ec;
